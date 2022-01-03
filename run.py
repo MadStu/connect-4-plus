f8e5fe7e544d9696b6b4dce0354b3faa0c49449c
Stuart Raynor <madstu@gmail.com>
2022-01-03 15:49:42 +0000

    Working out game board asthetics and potential animation

--- run.py
+++ run.py
@@ -1,7 +1,79 @@
+# Source code found at https://www.geeksforgeeks.org/clear-screen-python/
+# import only system from os
+from os import system, name
+  
+# import sleep to show output for some time period
+from time import sleep
+  
+# define our clear function
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
-print("| . | . | . | . | . | . | . |")
-print("| . | . | . | . | . | . | . |")
-print("| . | . | . | . | . | . | . |")
-print("| . | . | . | . | . | . | . |")
-print("| . | . | . | . | . | . | . |")
-print("| . | . | . | . | . | . | . |")
+clear ()
+print("               O                  ")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+
+sleep(0.2)
+clear ()
+print("                                  ")
+print("     | . | . | O | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+
+sleep(0.2)
+clear ()
+print("                                  ")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+
+sleep(0.2)
+clear ()
+print("                                  ")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+
+sleep(0.2)
+clear ()
+print("                                  ")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+
+sleep(0.2)
+clear ()
+print("                                  ")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | . | . | . | . | . |")
+print("     | . | . | O | . | . | . | . |")
+print("     | . | . | O | . | X | . | . |")
+print("                                  ")
+print("                                  ")
+print("                                  ")
