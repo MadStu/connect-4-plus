b3180b89e01c20326881e9ba19b4b4b9aad906fe
Stuart Raynor <madstu@gmail.com>
2022-01-05 14:49:42 +0000

    Refactor the game board code

--- run.py
+++ run.py
@@ -162,21 +162,29 @@ def game_board():
     clear()
     logo()
 
+    margin = "          "
+    space = "   "
+    wall = " | "
+
     print("             1   2   3   4   5   6   7  ")
-    print(
-        f"             {board_data[0][0]}   {board_data[1][0]}   {board_data[2][0]}   {board_data[3][0]}   {board_data[4][0]}   {board_data[5][0]}   {board_data[6][0]} ")
-    print(
-        f"           | {board_data[0][1]} | {board_data[1][1]} | {board_data[2][1]} | {board_data[3][1]} | {board_data[4][1]} | {board_data[5][1]} | {board_data[6][1]} |")
-    print(
-        f"           | {board_data[0][2]} | {board_data[1][2]} | {board_data[2][2]} | {board_data[3][2]} | {board_data[4][2]} | {board_data[5][2]} | {board_data[6][2]} |")
-    print(
-        f"           | {board_data[0][3]} | {board_data[1][3]} | {board_data[2][3]} | {board_data[3][3]} | {board_data[4][3]} | {board_data[5][3]} | {board_data[6][3]} |")
-    print(
-        f"           | {board_data[0][4]} | {board_data[1][4]} | {board_data[2][4]} | {board_data[3][4]} | {board_data[4][4]} | {board_data[5][4]} | {board_data[6][4]} |")
-    print(
-        f"           | {board_data[0][5]} | {board_data[1][5]} | {board_data[2][5]} | {board_data[3][5]} | {board_data[4][5]} | {board_data[5][5]} | {board_data[6][5]} |")
-    print(
-        f"           | {board_data[0][6]} | {board_data[1][6]} | {board_data[2][6]} | {board_data[3][6]} | {board_data[4][6]} | {board_data[5][6]} | {board_data[6][6]} |")
+
+    i = 0
+    while i < 7:
+        ii = 0
+        board_line = margin
+        while ii < 7:
+            if i == 0:
+                board_line = board_line + space
+            else:
+                board_line = board_line + wall
+            board_line = board_line + board_data[ii][i]
+            ii += 1
+        if i == 0:
+            board_line = board_line + space
+        else:
+            board_line = board_line + wall
+        print(board_line)
+        i += 1
     print("")
     if player_turn:
         print("                                    Your Turn")
